82fce76801e275919acb24b25e6bef237393c6b2
Lucas L. Pacheco <luccaslp99@gmail.com>
2022-09-15 19:21:16 -0300

    aula1

---
+++ aula1/aula1.js
@@ -0,0 +1,31 @@
+// concatenação com template string
+/*var name = 'Lucas'
+
+console.log(`meu nome é ${name}, e o seu?`)
+*/
+
+
+//objeto
+/*
+const person = {
+    name : 'John',
+    age: 30,
+    weight: 70.32,
+    isAdmin: true
+}
+
+console.log(person)
+*/
+
+//array
+
+// const animals =[
+//     'monkey',
+//     'tiger',
+//     {
+//         name: 'lion',
+//         age: 12
+//     }
+// ]
+
+// console.log(animals[2].age)
