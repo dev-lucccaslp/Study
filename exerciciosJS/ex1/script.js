02e2196c211a637d670bc1fc9ed54b5aaacf3e57
Lucas L. Pacheco <luccaslp99@gmail.com>
2022-10-12 19:16:36 -0300

    concluindo ex1

---
+++ exerciciosJS/ex1/script.js
@@ -0,0 +1,26 @@
+var myvar
+myvar = 10
+
+soma = 10 + 8
+
+soma = soma + 1
+
+soma = soma * 3
+
+//valor de soma é 57
+
+var souninja = true
+
+var comida = ['arroz', 'feijão', 'ovo']
+
+teste = typeof myvar == soma
+
+//myvar e soma nao são iguais
+res = myvar <= soma
+// myvar é menor que soma
+
+function divisao(a, b){
+    return a / b
+}
+
+console.log(divisao(10, 2))
